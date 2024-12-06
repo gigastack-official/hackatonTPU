d041787e19f24836f75bad650a78cd0babdfaf68
Vladimir Gorohov <vova-gorohov04@mail.ru>
2024-12-06 10:18:25 +0700

    Настроена БД и сделан requirements.txt

--- README.md
+++ README.md
@@ -3,14 +3,3 @@ python manage.py migrate
 
 
 python manage.py runserver
-
-
-Примените разрешение к представлению:
-
-python
-Копировать код
-class AdminOnlyView(generics.ListAPIView):
-    permission_classes = [IsAdminUser]
-    # ...
-
-operation_summary="Register a new user",
--- backendApp/settings.py
+++ backendApp/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'backendApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'authTemplateDjango',
+        'NAME': 'hacatonTPU',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
