a3cb7a087fcacec6f0743655a458500b5235c9ff
Vladimir Gorohov <vova-gorohov04@mail.ru>
2024-12-06 12:43:10 +0700

    CORS

--- backendApp/settings.py
+++ backendApp/settings.py
@@ -38,14 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     'django_filters',
     'api',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
